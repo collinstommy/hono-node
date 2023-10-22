150339ffdc642f19dd2eef918647870b8b3cf84f
collinstommy <tomascollins@gmail.com>
2023-10-22 19:36:41 -0400

    tailwind prose

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ export const Layout: FC = ({ children }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script src="https://unpkg.com/htmx.org@1.9.3"></script>
         <script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
-        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="https://cdn.tailwindcss.com?plugins=forms,typography"></script>
         <title>ToDo App</title>
       </head>
       <body class="border bg-gray-200 px-4 prose" hx-boost="true">
